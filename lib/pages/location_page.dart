540fe54a68efc7c83f10a4f16cda4e8ac3c5e1b1
efar716 <110000165+efar716@users.noreply.github.com>
2024-04-12 07:28:53 +1200

    Search bar

--- lib/pages/location_page.dart
+++ lib/pages/location_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/utilities/bottom_nav.dart';
 
+// Page for users to lookup weather based on location
 class LocationPage extends StatelessWidget {
   const LocationPage({super.key});
 
@@ -12,8 +13,29 @@ class LocationPage extends StatelessWidget {
         backgroundColor: Colors.deepPurple,
         elevation: 30.0,
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [SearchField()],
+        ),
+      ),
       bottomNavigationBar: BottomNav(),
     );
     ;
+  }
+}
+
+// Text area component to look up locations
+class SearchField extends StatelessWidget {
+  const SearchField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+          prefix: Icon(Icons.search),
+          filled: true,
+          labelText: "Search locations"),
+    );
   }
 }
